57a6ada6571ef46b1d00998458612c9918a73cfc
sagmaar <47426476+sagmaar@users.noreply.github.com>
2020-04-28 11:51:29 +0100

    Conditions removed test.py

--- test.py
+++ test.py
@@ -5,13 +5,3 @@ print (f"Hello, {name} !")
 
 i = 290
 print (f"I is equals to {i}")
-
-x = 50
-if x > 0:
-  print ("X is Lesser ")
-  
- elif x < 0:
-   print ("X is Greater")
-  
- else:
-   print ("X is neither no")
