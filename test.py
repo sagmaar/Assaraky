3e773d17760552e560384e58dd6db58cd73e4366
sagmaar <47426476+sagmaar@users.noreply.github.com>
2020-04-28 11:45:12 +0100

    Conditions added test.py

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ print (f"Hello, {name} !")
 
 i = 290
 print (f"I is equals to {i}")
+
+x = 50
+if x > 0:
+  print ("X is Greater than 0")
+  
+ elif x < 0:
+   print ("X is Lessthan 0")
+   
+   
+ else:
+   print ("X is neither nor ...")
